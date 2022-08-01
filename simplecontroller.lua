85879e6c165d6037d7f98baf5ddb77abfc6b3fb6
Sheehab Al-Islam <94201088+islamsheehab@users.noreply.github.com>
2022-08-01 15:30:19 -0400

    Update simplecontroller.lua

--- simplecontroller.lua
+++ simplecontroller.lua
@@ -4,3 +4,10 @@ local Controllers = {
     MonomialController  = {}, -- generalisation of linear, quadratic, ... controllers
 }
 
+-- Metatable-based polymorphism
+Controllers.MonomialController.__index = Controllers.MonomialController
+setmetatable(Controllers.MonomialController, Controllers)
+
+Controllers.__index = Controllers
+
+return Controllers
