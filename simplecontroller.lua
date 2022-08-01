6e3a4b02a5111d02e9ebab7562bb36b9257719eb
Sheehab Al-Islam <94201088+islamsheehab@users.noreply.github.com>
2022-08-01 15:32:11 -0400

    Update simplecontroller.lua

--- simplecontroller.lua
+++ simplecontroller.lua
@@ -10,4 +10,21 @@ setmetatable(Controllers.MonomialController, Controllers)
 
 Controllers.__index = Controllers
 
+function Controllers.MonomialController.new(degree: number, strength: number)
+    degree = degree or 1
+    strength = strength or 1
+
+    local self = {
+        Degree    = degree,
+        Strength  = strength,
+
+        Goal      = 0,
+        Current   = 0,
+
+        PreviousTime = os.clock()
+    }
+
+    return setmetatable(self, Controllers.MonomialController)
+end
+
 return Controllers
